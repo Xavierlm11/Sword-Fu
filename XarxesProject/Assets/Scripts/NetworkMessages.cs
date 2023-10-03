a06837ccaa58385633f4b751b12cf09cc6d6956f
Xavier Casadó <79162780+Akage369@users.noreply.github.com>
2023-10-03 12:00:36 +0200

    Functional Input Fields

---
+++ XarxesProject/Assets/Scripts/NetworkMessages.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NetworkMessages : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField ipField;
+
+    [SerializeField]
+    private TMP_InputField messageField;
+
+    [SerializeField]
+    private string ip;
+
+    [SerializeField]
+    private string message;
+
+    public void SetRemoteIP()
+    {
+        ip = ipField.text;
+    }
+
+    public void SetMessage()
+    {
+        message = messageField.text;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetRemoteIP();
+            SetMessage();
+        }
+    }
+}
